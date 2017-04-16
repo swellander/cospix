ebc94811755aedaf0f5b9eb70fe6f91a18021112
Samuel Wellander <samwellander@gmail.com>
2017-04-16 10:21:05 -0700

    Add paperclip config to test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,16 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('cospix'),
+    access_key_id: ENV.fetch('AKIAIUL6SBPRQFINC2SQ'),
+    secret_access_key: ENV.fetch('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'),
+    s3_region: ENV.fetch('Oregon'),
+  }
+}
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
